93a87a009dcfd05f4eff00500937c4f2a5ccfaa5
frederick <frederickhidayat2003@gmail.com>
2023-11-18 21:10:11 +0700

    feat: vue js breeze trial

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './resources/js/**/*.vue',
     ],
 
     theme: {
@@ -14,9 +15,6 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
-            backgroundColor: {
-                'blue-200': '#your-sky-blue-color-code',
-              },
         },
     },
 
